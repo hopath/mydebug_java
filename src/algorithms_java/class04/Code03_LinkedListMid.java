b4a1a2c22a814f10407a912714e358bdf73bb50e
hopath <zhangzhiwei.hopath@outlook.com>
2024-05-16 11:36:17 +0800

    update class04

--- src/algorithms_java/class04/Code03_LinkedListMid.java
+++ src/algorithms_java/class04/Code03_LinkedListMid.java
@@ -6,4 +6,214 @@ package algorithms_java.class04;
  */
 public class Code03_LinkedListMid {
 
+    static class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+
+    public static void printLinkedList(Node head) {
+        while (head != null) {
+            System.out.print(head.value + " ");
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node test = new Node(1);
+        Node cur = test;
+        for (int i = 2; i < 5; i++) {
+            cur.next = new Node(i);
+            cur = cur.next;
+        }
+        //System.out.println(getOddMidOrEvenLeftMid(test));
+        //System.out.println(getOddMidOrEvenRightMid(test));
+        //System.out.println(getOddLeftMidOrEvenPreLeftMid(test));
+        System.out.println(getOddLeftMidOrEvenPreRightMid(test));
+        printLinkedList(test);
+    }
+
+    public static Node getOddMidOrEvenLeftMid(Node head) {
+        int cnt = 0;
+
+        Node cur = head;
+        while (cur != null) {
+            cnt++;
+            cur = cur.next;
+        }
+
+        if (cnt % 2 == 0) {
+            return getEvenLinkedListLeftMid(head);
+        }
+
+        return getOddLinkedListMid(head);
+    }
+
+    public static Node getOddMidOrEvenRightMid(Node head) {
+        int cnt = 0;
+
+        Node cur = head;
+        while (cur != null) {
+            cnt++;
+            cur = cur.next;
+        }
+
+        if (cnt % 2 == 0) {
+            return getEvenLinkedListRightMid(head);
+        }
+
+        return getOddLinkedListMid(head);
+    }
+
+    public static Node getOddLeftMidOrEvenPreLeftMid(Node head) {
+        int cnt = 0;
+
+        Node cur = head;
+        while (cur != null) {
+            cnt++;
+            cur = cur.next;
+        }
+
+        if (cnt % 2 == 0) {
+            return getEvenLinkedListPreLeftMid(head);
+        }
+
+        return getOddLinkedListLeftMid(head);
+    }
+
+    public static Node getOddLeftMidOrEvenPreRightMid(Node head) {
+        int cnt = 0;
+
+        Node cur = head;
+        while (cur != null) {
+            cnt++;
+            cur = cur.next;
+        }
+
+        if (cnt % 2 == 0) {
+            return getEvenLinkedListPreRightMid(head);
+        }
+
+        return getOddLinkedListLeftMid(head);
+    }
+
+    public static Node getOddLinkedListMid(Node head) {
+        if (head.next == null || head == null) {
+            return head;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public static Node getEvenLinkedListLeftMid(Node head) {
+        if (head.next.next == null || head == null) {
+            return head;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public static Node getEvenLinkedListRightMid(Node head) {
+        if (head.next.next == null) {
+            return head.next;
+        }
+        if (head.next == null) {
+            return null;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public static Node getOddLinkedListLeftMid(Node head) {
+        if (head.next == null || head == null) {
+            return null;
+        }
+
+        Node pre = null;
+        Node slow = head;
+        Node fast = head;
+
+        while (fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return pre;
+    }
+
+    public static Node getEvenLinkedListPreLeftMid(Node head) {
+        if (head.next.next == null || head == null) {
+            return null;
+        }
+
+        Node pre = null;
+        Node slow = head;
+        Node fast = head;
+
+        while (fast.next.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return pre;
+    }
+
+    public static Node getEvenLinkedListPreRightMid(Node head) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next.next == null) {
+            return head;
+        }
+
+        Node pre = null;
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return pre;
+    }
+
 }
--- src/algorithms_java/class04/Code04_isPalindromeLikedList.java
+++ src/algorithms_java/class04/Code04_isPalindromeLikedList.java
@@ -6,4 +6,76 @@ package algorithms_java.class04;
  */
 public class Code04_isPalindromeLikedList {
 
+    public static void main(String[] args) {
+        Code03_LinkedListMid.Node test = new Code03_LinkedListMid.Node(1);
+        test.next = new Code03_LinkedListMid.Node(2);
+        test.next.next = new Code03_LinkedListMid.Node(3);
+        test.next.next.next = new Code03_LinkedListMid.Node(4);
+        test.next.next.next.next = new Code03_LinkedListMid.Node(5);
+        test.next.next.next.next.next = new Code03_LinkedListMid.Node(6);
+        test.next.next.next.next.next.next = new Code03_LinkedListMid.Node(7);
+        test.next.next.next.next.next.next.next = new Code03_LinkedListMid.Node(8);
+
+        Code03_LinkedListMid.printLinkedList(test);
+        System.out.println();
+        insertEqualNode(test);
+        Code03_LinkedListMid.printLinkedList(test);
+    }
+    public static boolean isPalindromeLikedList(Code03_LinkedListMid.Node head){
+        if(head == null || head.next == null || head.next.next == null){
+            return true;
+        }
+        Code03_LinkedListMid.Node mid = Code03_LinkedListMid.getOddMidOrEvenLeftMid(head);
+        Code03_LinkedListMid.Node pre = mid;
+        Code03_LinkedListMid.Node cur = mid.next;
+        Code03_LinkedListMid.Node next = null;
+        mid.next = null;
+
+        //反转链表
+        while(cur != null){
+            //起保存下一个节点的作用，关注于cur,pre若已经反转完成也就没必要保存了
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        //从左向右，从右向左同时遍历，若遍历过程中左右对应值不同则返回false
+        while(head != null){
+            if(head.value != pre.value){
+                return false;
+            }
+            head = head.next;
+            pre = pre.next;
+        }
+
+        return true;
+    }
+
+    public static void insertEqualNode(Code03_LinkedListMid.Node head){
+        Code03_LinkedListMid.Node mid = Code03_LinkedListMid.getOddMidOrEvenLeftMid(head);
+        Code03_LinkedListMid.Node pre = mid;
+        Code03_LinkedListMid.Node cur = mid.next;
+        Code03_LinkedListMid.Node lNext = null;
+        Code03_LinkedListMid.Node rNext = null;
+        mid.next = null;
+
+        //反转链表
+        while(cur != null){
+            //起保存下一个节点的作用，关注于cur,pre若已经反转完成也就没必要保存了
+            lNext = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = lNext;
+        }
+
+        while(head != null && head != pre){
+            lNext = head.next;
+            rNext = pre.next;
+            head.next = pre;
+            pre.next = lNext;
+            head = lNext;
+            pre = rNext;
+        }
+    }
 }
