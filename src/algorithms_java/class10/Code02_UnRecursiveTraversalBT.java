8cd47acfdd52567e302841002bb56a1c0531a80d
hopath <zhangzhiwei.hopath@outlook.com>
2024-06-06 10:01:51 +0800

    commit class10

--- src/algorithms_java/class10/Code02_UnRecursiveTraversalBT.java
+++ src/algorithms_java/class10/Code02_UnRecursiveTraversalBT.java
@@ -9,16 +9,17 @@ import java.util.Stack;
 public class Code02_UnRecursiveTraversalBT {
 
     public static void main(String[] args) {
-        TNode root = new TNode(3);
+        TNode root = new TNode(1);
         root.left = new TNode(2);
-        root.right = new TNode(1);
+        root.right = new TNode(3);
         root.left.left = new TNode(4);
-        root.left.right = new TNode(5);
-        root.right.left = new TNode(8);
-        root.right.right = new TNode(9);
         root.left.left.left = new TNode(10);
+        root.right.right = new TNode(7);
+        root.right.right.left = new TNode(8);
+        root.left.left.right = new TNode(5);
+        root.left.left.right.left = new TNode(6);
 
-       pos(root);
+        pos(root);
     }
     static class TNode{
         TNode left;
@@ -40,77 +41,35 @@ public class Code02_UnRecursiveTraversalBT {
     public static void pre(TNode tNode){
         Stack<TNode> stack = new Stack<TNode>();
         TNode cur = tNode;
-
-        while(cur != null){
-            System.out.print(cur.value + " ");
-            stack.push(cur);
-            cur = cur.left;
-        }
-
+        stack.push(cur);
 
         while(!stack.empty()){
-            TNode pop = stack.pop();
-            if(pop.right != null){
-                System.out.print(pop.right.value + " ");
-            }
-        }
-
-        cur = tNode.right;
-        while(cur != null){
-            if(cur != tNode.right){
+            while(cur != null){
                 System.out.print(cur.value + " ");
+                stack.push(cur);
+                cur = cur.left;
             }
-            stack.push(cur);
-            cur = cur.left;
-        }
-
-        while(!stack.empty()){
             TNode pop = stack.pop();
-            if(pop.right != null){
-                System.out.print(pop.right.value + " ");
-            }
+            cur = pop.right;
         }
     }
 
     public static void pos(TNode tNode){
         Stack<TNode> stack = new Stack<>();
-
         TNode cur = tNode;
-        while(cur != null){
-            stack.push(cur);
-            cur = cur.left;
-        }
+        stack.push(cur);
 
-        TNode pop = null;
-        while(pop != tNode){
-            pop = stack.pop();
-            if(pop.left != null){
-                System.out.print(pop.left.value + " ");
+        while(!stack.isEmpty()){
+            while(cur != null){
+                if(cur != tNode){
+                    stack.push(cur);
+                }
+                cur = cur.left;
             }
-            if(pop.right != null){
-                System.out.print(pop.right.value + " ");
-            }
+            TNode pop = stack.pop();
+            System.out.print(pop.value + " ");
+            cur = pop.right;
         }
-
-        stack.push(pop);
-        cur = tNode.right;
-        while(cur != null){
-            stack.push(cur);
-            cur = cur.left;
-        }
-
-        pop = stack.pop();
-        while(pop != tNode){
-            if(pop.left != null){
-                System.out.print(pop.left.value + " ");
-            }
-            if(pop.right != null){
-                System.out.print(pop.right.value + " ");
-            }
-            pop = stack.pop();
-        }
-
-        System.out.print(tNode.value);
     }
 
 }
