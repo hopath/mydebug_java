be7b6b1c8b67b7ac68a3acd4f4e403cd0052fecd
hopath <zhangzhiwei.hopath@outlook.com>
2024-09-06 11:37:13 +0800

    update

--- src/algorithms_java/class14/Code08_Dijkstra.java
+++ src/algorithms_java/class14/Code08_Dijkstra.java
@@ -53,45 +53,123 @@ public class Code08_Dijkstra {
         return hashMap;
     }
 
-    static class tNode {
-        Node node;
-        int distance;
 
-        public tNode() {
-        }
+    public static class NodeRecord {
+        public Node node;
+        public int distance;
 
-        public tNode(Node node, int distance) {
+        public NodeRecord(Node node, int distance) {
             this.node = node;
             this.distance = distance;
         }
     }
 
-    static class MyComparator implements Comparator<tNode> {
+    public static class NodeHeap {
+        private Node[] nodes; // 实际的堆结构
+        // key 某一个node， value 上面堆中的位置
+        private HashMap<Node, Integer> heapIndexMap;
+        // key 某一个节点， value 从源节点出发到该节点的目前最小距离
+        private HashMap<Node, Integer> distanceMap;
+        private int size; // 堆上有多少个点
 
-        @Override
-        public int compare(tNode o1, tNode o2) {
-            return o1.distance - o2.distance;
+        public NodeHeap(int size) {
+            nodes = new Node[size];
+            heapIndexMap = new HashMap<>();
+            distanceMap = new HashMap<>();
+            size = 0;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        // 有一个点叫node，现在发现了一个从源节点出发到达node的距离为distance
+        // 判断要不要更新，如果需要的话，就更新
+        public void addOrUpdateOrIgnore(Node node, int distance) {
+            if (inHeap(node)) {
+                distanceMap.put(node, Math.min(distanceMap.get(node), distance));
+                insertHeapify(node, heapIndexMap.get(node));
+            }
+            if (!isEntered(node)) {
+                nodes[size] = node;
+                heapIndexMap.put(node, size);
+                distanceMap.put(node, distance);
+                insertHeapify(node, size++);
+            }
+        }
+
+        public NodeRecord pop() {
+            NodeRecord nodeRecord = new NodeRecord(nodes[0], distanceMap.get(nodes[0]));
+            swap(0, size - 1);
+            heapIndexMap.put(nodes[size - 1], -1);
+            distanceMap.remove(nodes[size - 1]);
+            // free C++同学还要把原本堆顶节点析构，对java同学不必
+            nodes[size - 1] = null;
+            heapify(0, --size);
+            return nodeRecord;
+        }
+
+        private void insertHeapify(Node node, int index) {
+            while (distanceMap.get(nodes[index]) < distanceMap.get(nodes[(index - 1) / 2])) {
+                swap(index, (index - 1) / 2);
+                index = (index - 1) / 2;
+            }
+        }
+
+        private void heapify(int index, int size) {
+            int left = index * 2 + 1;
+            while (left < size) {
+                int smallest = left + 1 < size && distanceMap.get(nodes[left + 1]) < distanceMap.get(nodes[left])
+                        ? left + 1
+                        : left;
+                smallest = distanceMap.get(nodes[smallest]) < distanceMap.get(nodes[index]) ? smallest : index;
+                if (smallest == index) {
+                    break;
+                }
+                swap(smallest, index);
+                index = smallest;
+                left = index * 2 + 1;
+            }
+        }
+
+        private boolean isEntered(Node node) {
+            return heapIndexMap.containsKey(node);
+        }
+
+        private boolean inHeap(Node node) {
+            return isEntered(node) && heapIndexMap.get(node) != -1;
+        }
+
+        private void swap(int index1, int index2) {
+            heapIndexMap.put(nodes[index1], index2);
+            heapIndexMap.put(nodes[index2], index1);
+            Node tmp = nodes[index1];
+            nodes[index1] = nodes[index2];
+            nodes[index2] = tmp;
         }
     }
 
-    public static List<tNode> dijkstra2(Node from) {
+    public static HashMap<Node, Integer> dijkstra2(Node from, int size) {
+        NodeHeap nodeHeap = new NodeHeap(size);
+        HashMap<Node, Integer> res = new HashMap<>();
 
-        Code02_HeapGreater<tNode> heapGreater =
-                new Code02_HeapGreater<tNode>(new ArrayList<tNode>(), new HashMap<tNode, Integer>(), new MyComparator());
+        nodeHeap.addOrUpdateOrIgnore(from, 0);
 
-
-        ArrayList<tNode> res = new ArrayList<>();
-
-        heapGreater.push(new tNode(from, 0));
-
-        while (!heapGreater.isEmpty()) {
-            tNode pop = heapGreater.pop();
+        while (!nodeHeap.isEmpty()) {
+            NodeRecord pop = nodeHeap.pop();
+            Node node = pop.node;
             int distance = pop.distance;
 
-            for (Edge edge : pop.node.edges) {
-
+            for (Edge edge : node.edges) {
+                distance = distance + edge.weight;
+                Node to = edge.to;
+                nodeHeap.addOrUpdateOrIgnore(to, distance);
             }
+
+            res.put(node, distance);
         }
+
+        return res;
     }
 
 }
