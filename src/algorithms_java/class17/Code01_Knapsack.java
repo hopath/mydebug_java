92c7242e15c88977f8f8c958d537346264efaca4
hopath <zhangzhiwei.hopath@outlook.com>
2024-09-21 12:01:08 +0800

    update

--- src/algorithms_java/class17/Code01_Knapsack.java
+++ src/algorithms_java/class17/Code01_Knapsack.java
@@ -1,10 +1,21 @@
 package algorithms_java.class17;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author 张志伟
  * @version 1.0
  */
 public class Code01_Knapsack {
+
+    @Test
+    public void test() {
+        int[] w = {3, 4, 8, 9, 7};
+        int[] v = {4, 6, 9, 1, 3};
+
+        System.out.println(getMaxValue(w, v, 20));
+        System.out.println(dp(w, v, 20));
+    }
 
     public static int getMaxValue(int[] w, int[] v, int bag) {
         if (w == null || w.length == 0 || w.length != v.length || v == null || v.length == 0) {
@@ -14,19 +25,19 @@ public class Code01_Knapsack {
         return process01(w, v, 0, bag);
     }
 
-    public static int process01(int[] w, int[] v, int index, int bag) {
-        if (bag < 0) {
+    public static int process01(int[] w, int[] v, int index, int rest) {
+        if (rest < 0) {
             return -1;
         }
 
         if (index == w.length) {
             return 0;
         }
-        int p1 = process01(w, v, index + 1, bag);
+        int p1 = process01(w, v, index + 1, rest);
 
         int p2 = 0;
         //若后续无效则返回-1
-        int next = process01(w, v, index + 1, bag - w[index]);
+        int next = process01(w, v, index + 1, rest - w[index]);
         if (next != -1) {
             p2 = v[index] + next;
         }
@@ -34,4 +45,28 @@ public class Code01_Knapsack {
         return Math.max(p1, p2);
     }
 
+
+    public static int dp(int[] w, int[] v, int bag) {
+        if (w == null || w.length == 0 || w.length != v.length || v == null || v.length == 0) {
+            return 0;
+        }
+
+        int N = w.length;
+        int[][] dp = new int[N + 1][bag + 1];
+
+        for (int index = N - 1; index >= 0; index--) {
+            for (int rest = 0; rest <= bag; rest++) {
+                int p1 = dp[index + 1][rest];
+                int p2 = 0;
+                int next = rest - w[index] < 0 ? -1 : dp[index + 1][rest - w[index]];
+                if (next != -1) {
+                    p2 = v[index] + next;
+                }
+
+                dp[index][rest] = Math.max(p1, p2);
+            }
+        }
+
+        return dp[0][bag];
+    }
 }
