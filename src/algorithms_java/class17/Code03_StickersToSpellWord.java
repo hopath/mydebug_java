c18e2618313c6acda784dab5248c70a8432300a9
hopath <zhangzhiwei.hopath@outlook.com>
2024-09-26 11:58:28 +0800

    update

--- src/algorithms_java/class17/Code03_StickersToSpellWord.java
+++ src/algorithms_java/class17/Code03_StickersToSpellWord.java
@@ -1,7 +1,8 @@
 package algorithms_java.class17;
 
-import com.sun.xml.internal.bind.v2.util.StackRecorder;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
 
 /**
  * @author 张志伟
@@ -12,93 +13,94 @@ public class Code03_StickersToSpellWord {
     @Test
     public void test() {
         String[] strs = new String[]{"abc", "csd", "dbc"};
-        System.out.println(minStick(strs, "dscbda"));
-        System.out.println(minStick02(strs, "dscbda"));
+        System.out.println(minStick(strs, "dsca"));
+        System.out.println(minStack03(strs, "dsca"));
     }
 
 
-    public static int minStick02(String[] strs, String target) {
+    public static int minStick02(String[] stickers, String target) {
         if (target == null || target.length() == 0) {
             return 0;
         }
 
-        if (strs == null || strs.length == 0) {
+        if (stickers == null || stickers.length == 0) {
             return -1;
         }
 
         //对strs进行词频统计
-        int N = strs.length;
+        int N = stickers.length;
         int[][] sticks = new int[N][26];
         for (int i = 0; i < N; i++) {
-            char[] chars = strs[i].toCharArray();
+            char[] chars = stickers[i].toCharArray();
             for (char ch : chars) {
                 sticks[i][ch - 'a']++;
             }
         }
 
-        return process02(sticks, target);
+        int ans = process02(sticks, target);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 
-    public static int process02(int[][] sticks, String target) {
-        if (target.length() == 0) {
+    public static int process02(int[][] stickers, String t) {
+        if (t.length() == 0) {
             return 0;
         }
 
         //对target进行词频统计
-        char[] targetCharArray = target.toCharArray();
+        char[] target = t.toCharArray();
         int[] tcounts = new int[26];
-        for (char ch : targetCharArray) {
+        for (char ch : target) {
             tcounts[ch - 'a']++;
         }
 
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i < sticks.length; i++) {
-            StringBuffer stringBuffer = new StringBuffer();
-            if (sticks[i][targetCharArray[0] - 'a'] > 0) {
+        for (int i = 0; i < stickers.length; i++) {
+            int[] stick = stickers[i];
+            if (stick[target[0] - 'a'] > 0) {
+                StringBuilder builder = new StringBuilder();
                 for (int j = 0; j < 26; j++) {
-                    int nums = sticks[i][j] - tcounts[j];
-                    for (int k = 0; k < nums; k++) {
-                        stringBuffer.append((char) (j + 'a'));
+                    if (tcounts[j] > 0) {
+                        int nums = tcounts[j] - stick[j];
+                        for (int k = 0; k < nums; k++) {
+                            builder.append((char) (j + 'a'));
+                        }
                     }
                 }
-            }
-
-            String rest = stringBuffer.toString();
-            if(rest.length() != target.length()){
-                Math.min(min, process02(sticks, rest));
+                String rest = builder.toString();
+                min = Math.min(min, process02(stickers, rest));
             }
         }
-
-        return min == Integer.MAX_VALUE ? -1 : min + 1;
+        return min + (min == Integer.MAX_VALUE ? 0 : 1);
     }
 
-    public static int minStick(String[] strs, String target) {
+    public static int minStick(String[] stickers, String target) {
         if (target == null || target.length() == 0) {
             return 0;
         }
 
-        if (strs == null || strs.length == 0) {
+        if (stickers == null || stickers.length == 0) {
             return -1;
         }
 
-        return process01(strs, target);
+        int ans = process01(stickers, target);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 
-    public static int process01(String[] strs, String target) {
+    public static int process01(String[] stickers, String target) {
         if (target.length() == 0) {
             return 0;
         }
 
         int min = Integer.MAX_VALUE;
 
-        for (String first : strs) {
-            String rest = minus(first, target);
+        for (String first : stickers) {
+            String rest = minus(target, first);
             if (target.length() != rest.length()) {
-                min = Math.min(min, process01(strs, rest));
+                min = Math.min(min, process01(stickers, rest));
             }
         }
 
-        return min == Integer.MAX_VALUE ? -1 : min + 1;
+        return min + (min == Integer.MAX_VALUE ? 0 : 1);
     }
 
 
@@ -116,17 +118,67 @@ public class Code03_StickersToSpellWord {
         }
 
 
-        StringBuffer stringBuffer = new StringBuffer();
+        StringBuilder builder = new StringBuilder();
 
         for (int i = 0; i < count.length; i++) {
             while (count[i] > 0) {
-                stringBuffer.append((char) (i + 'a'));
+                builder.append((char) (i + 'a'));
                 count[i]--;
             }
         }
 
-        return stringBuffer.toString();
+        return builder.toString();
     }
 
 
+    public static int minStack03(String[] stickers, String target) {
+        //对strs进行词频统计
+        int N = stickers.length;
+        int[][] sticks = new int[N][26];
+        for (int i = 0; i < N; i++) {
+            char[] chars = stickers[i].toCharArray();
+            for (char ch : chars) {
+                sticks[i][ch - 'a']++;
+            }
+        }
+
+        HashMap<String, Integer> dp = new HashMap<>();
+        dp.put("", 0);
+        int ans = process03(sticks, target, dp);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+
+    public static int process03(int[][] stickers, String t, HashMap<String, Integer> dp) {
+        if (dp.containsKey(t)) {
+            return dp.get(t);
+        }
+        //对target进行词频统计
+        char[] target = t.toCharArray();
+        int[] tcounts = new int[26];
+        for (char ch : target) {
+            tcounts[ch - 'a']++;
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < stickers.length; i++) {
+            int[] stick = stickers[i];
+            if (stick[target[0] - 'a'] > 0) {
+                StringBuilder builder = new StringBuilder();
+                for (int j = 0; j < 26; j++) {
+                    if (tcounts[j] > 0) {
+                        int nums = tcounts[j] - stick[j];
+                        for (int k = 0; k < nums; k++) {
+                            builder.append((char) (j + 'a'));
+                        }
+                    }
+                }
+                String rest = builder.toString();
+                min = Math.min(min, process02(stickers, rest));
+            }
+        }
+        int ans = min + (min == Integer.MAX_VALUE ? 0 : 1);
+        dp.put(t, ans);
+        return ans;
+    }
 }
